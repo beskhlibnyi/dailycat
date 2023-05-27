65c38503211a4e7f51a576216ca343b9c1e343fe
beskhlibnyi <beskhlibnyi@gmail.com>
2023-05-27 19:49:54 +0300

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,11 @@
+require_relative "boot"
+require "rails/all"
+
+Bundler.require(*Rails.groups)
+
+module DailyCat
+  class Application < Rails::Application
+    config.load_defaults 7.0
+    config.api_only = true
+  end
+end
